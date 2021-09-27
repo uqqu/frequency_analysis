5963885da4624522d275a9852de71c7020be8057
pas <mihnevich.pavel.d@gmail.com>
2021-09-27 22:48:04 +0300

    add English pre-analysis

--- examples/multiUN_analysis/result.py
+++ examples/multiUN_analysis/result.py
@@ -3,6 +3,6 @@
 import frequency_analysis
 
 with frequency_analysis.Result() as res:
-    res.treat(limits=[100]*4, min_quantity=[20]*5)
+    res.treat(limits=[1000]*4, min_quantity=[10]*5)
     res.sheet_custom_symb(ascii_lowercase)
     res.sheet_en_symb_bigrams()
